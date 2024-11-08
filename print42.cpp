e9cb468f799f2714318a1c07a7f9a894cca2a383
jerperez42 <160843311+jerperez42@users.noreply.github.com>
2024-11-08 09:34:19 +0100

    ini

---
+++ print42.cpp
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print42.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jerperez <jerperez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 10:21:53 by jerperez          #+#    #+#             */
+/*   Updated: 2024/11/08 09:32:31 by jerperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <iostream>
+# include <cstring>
+# include <map>
+
+
+typedef std::map<std::string, std::string> t_map;
+// static void	_print_help(void)
+// {
+// 	std::cout << "manual" << std::endl;
+// }
+
+
+static int	_updateArgs(std::string pair, t_map &map)
+{
+	std::size_t pos = pair.find("=");
+
+	if (std::string::npos == pos)
+		return 1;
+	std::string const key = pair.substr(0, pos);
+	std::string const value = pair.substr(pos + 1, pair.length());
+
+	pos = value.find("=");
+	if (std::string::npos != pos)
+		return 1;
+	map[key] = value;
+	return 0;
+}
+
+
+static int	_parseArgs(int ac, char *av[], t_map &map)
+{
+	for (int i = 1; i != ac; ++i)
+	{
+		if (_updateArgs(av[i], map))
+			return 1;
+	}
+	return 0;
+}
+
+
+static void _printArgs(t_map &map)
+{
+	t_map::const_iterator const	it_end = map.end();
+
+	for (t_map::iterator it = map.begin(); it != it_end; ++it)
+		std::cerr << it->first << ':' << it->second << "|" << std::endl;
+}
+
+
+static int	_get_score(int ac, char *av[])
+{
+	int score = 0;
+	for (int i = 0; i != ac; ++i)
+	{
+		score += std::strlen(av[i]);
+	}
+	return score;
+}
+
+static void	_print_msg(int ac, char *av[], char *env[])
+{
+	(void)env;
+	std::cout << 42 << std::endl;
+	//std::cerr << "info: your score is " << _get_score(ac, av) << ',' << ac << std::endl;
+	std::cerr << "info: your score is " << _get_score(ac, av) << std::endl;
+}
+
+int	main(int ac, char *av[], char *env[])
+{
+	t_map map;
+	if (_parseArgs(ac, av, map))
+		return 1;
+	_printArgs(map);
+	_print_msg(ac, av, env);
+	return 0;
+}
---
+++ print42.x
@@ -0,0 +1,38 @@
+NAME
+    The name of the command or function, followed by a one-line description of what it does.
+SYNOPSIS
+    In the case of a command, you get a formal description of how to run it and what command line options it takes.
+DESCRIPTION
+    A textual description of the functioning of the command or function.
+EXAMPLES
+    Some examples of common usage.
+SEE ALSO
+    A list of related commands or functions.
+BUGS
+    List known bugs.
+AUTHOR
+   Specify your contact information.
+COPYRIGHT
+    Specify your copyright information
+
+
+-a
+Adds 1
+
+--toggle-log=(on, off)
+Toggles log in /tmp
+
+-ns
+Do not substract 2 if -a is on
+
+-mood=(groovy)
+decrease the score as specific counts.
+
+--difficulty=(easy, medium, hard)
+sets the difficulty
+
+--disable-politeness
+switch the toggle to print "Hey, here is your answer [...] you are welcome."
+
+--allow-size=
+

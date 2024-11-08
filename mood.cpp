4b1fbeb9448fa3ef4fbd1d7a300b0ddf91140c88
김동엽 <gimdong-yeob@gimdong-yeob-ui-MacBookPro.local>
2024-11-08 11:45:03 +0100

    translate as korean

--- mood.cpp
+++ mood.cpp
@@ -15,7 +15,7 @@
 
 void	set_mood(t_map &map, std::string &s)
 {
-	std::string mood  = map["--mood"];
+	std::string mood  = map["--기분"];
 
 	int pos = s.length() - 1;
 	std::string s_out;
@@ -27,11 +27,11 @@ void	set_mood(t_map &map, std::string &s)
 	else
 		s_out=s.substr(0, pos);
 
-	if ("default" == mood)
+	if ("default" == mood || "기본" == mood)
 		not_changed = true;
-	else if ("idk" == mood )
+	else if ("idk" == mood || "신남" == mood)
 		s = "\e[1;6;30;45m" + s_out + "\e[0m";
-	else if ("chill" == mood)
+	else if ("chill" == mood || "나태" == mood)
 		s = "\e[94m" + s_out + "\e[0m";
 	else if ("" == mood)
 		s = "\e[0m" + s_out + "\e[0m";
